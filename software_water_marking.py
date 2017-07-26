c8cbd57cae0cf430333922d6bb6068286c427adf
Harashiyo <shohei.h1024@gmail.com>
2017-07-26 22:14:16 +0900

    Graphクラス追加

--- software_water_marking.py
+++ software_water_marking.py
@@ -4,39 +4,49 @@ u"""Software Water Marking.
 """
 
 
-def gc(V, E):
+class Graph:
+    u"""Graph Class."""
+    vertex = []
+    edge = []
+    num = 0
+
+    def __init__(self, num, edge):
+        self.num = num
+        self.__init__vertex()
+        self.edge += edge
+
+    def __init__vertex(self):
+        for i in range(self.num):
+            self.vertex.append(i + 1)
+
+
+def gc(graph):
     u"""GC Algorithm.
-    @param V vertex
-    @param E edge
     """
-    N = len(V)
-    result = [0] * N
+    result = [0] * graph.num
     color = 1
-    for i in range(N):
+    for i in range(graph.num):
         if result[i] == 0:
             result[i] = color
-            for j in range(i + 1, N):
-                if result[j] == 0 and check_color(result, E, color, j):
+            for j in range(i + 1, graph.num):
+                if result[j] == 0 and check_color(result, graph.edge, color, j):
                     result[j] = color
             color += 1
     return result
 
 
-def check_color(V, E, C, N):
+def check_color(vertex, edge, color, num):
     u"""Determine whether vertices can be colored.
-    @param V vertex
-    @param E edge
-    @param c color
-    @param N vertex number
     """
-    for i in range(N):
-        if V[i] == C and [i, N] in E:
+    for i in range(num):
+        if vertex[i] == color and [i, num] in edge:
             return False
     return True
 
 
 if __name__ == '__main__':
-    V = [1, 2, 3, 4, 5]
-    E = [[0, 1], [1, 3], [1, 4], [2, 3], [2, 4]]
-    re = gc(V, E)
-    print(re)
+    e = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [1, 2], [1, 3],
+         [1, 4], [1, 5], [1, 6], [2, 3], [2, 4], [2, 5], [2, 6], [5, 6]]
+    g = Graph(7, e)
+    g.vertex = gc(g)
+    print(g.vertex)
