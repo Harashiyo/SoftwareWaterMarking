1ae700323220f5735e0d79bfb088bc4db39a250d
Harashiyo <shohei.h1024@gmail.com>
2017-07-26 23:03:27 +0900

    gc引数修正

--- software_water_marking.py
+++ software_water_marking.py
@@ -6,40 +6,41 @@ u"""Software Water Marking.
 
 class Graph:
     u"""Graph Class."""
-    vertex = []
-    edge = []
-    num = 0
 
     def __init__(self, num, edge):
         self.num = num
-        self.__init__vertex()
-        self.edge += edge
-
-    def __init__vertex(self):
+        self.vertex = [0] * num
         for i in range(self.num):
-            self.vertex.append(i + 1)
+            self.vertex[i] = i + 1
+        self.edge = edge[:]
+        self.edge.sort()
+
+    def set_zero_vertex(self):
+        for i in range(self.num):
+            self.vertex[i] = 0
 
 
 def gc(graph):
     u"""GC Algorithm.
     """
-    result = [0] * graph.num
+    g = Graph(graph.num, graph.edge)
+    g.set_zero_vertex()
     color = 1
-    for i in range(graph.num):
-        if result[i] == 0:
-            result[i] = color
-            for j in range(i + 1, graph.num):
-                if result[j] == 0 and check_color(result, graph.edge, color, j):
-                    result[j] = color
+    for i in range(g.num):
+        if g.vertex[i] == 0:
+            g.vertex[i] = color
+            for j in range(i + 1, g.num):
+                if g.vertex[j] == 0 and check_color(g, color, j):
+                    g.vertex[j] = color
             color += 1
-    return result
+    return g
 
 
-def check_color(vertex, edge, color, num):
+def check_color(graph, color, num):
     u"""Determine whether vertices can be colored.
     """
     for i in range(num):
-        if vertex[i] == color and [i, num] in edge:
+        if graph.vertex[i] == color and [i, num] in graph.edge:
             return False
     return True
 
@@ -48,5 +49,5 @@ if __name__ == '__main__':
     e = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [1, 2], [1, 3],
          [1, 4], [1, 5], [1, 6], [2, 3], [2, 4], [2, 5], [2, 6], [5, 6]]
     g = Graph(7, e)
-    g.vertex = gc(g)
-    print(g.vertex)
+    gra = gc(g)
+    print(gra.vertex)
